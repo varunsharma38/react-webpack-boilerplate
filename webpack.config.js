3a1f6e5e1e155d386e2624527443a977180a1953
Varun Sharma <varunsharma38@live.com>
2019-11-02 13:34:45 +0530

    add style config

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,34 @@ module.exports = {
                 }
             },
             {
+				test: /\.(css|scss)$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+            },
+            {
+				test: /\.(jpg|jpeg|png|svg|gif)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: { limit: 40000 }
+						/*
+						>40000 bytes -- separate file is created
+						<40000 bytes -- image is converted to base64 and added to bundle.js
+						*/
+					},
+					'image-webpack-loader'
+				]	
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			},
+            {
                 test: /\.html$/,
                 use: {
                     loader: 'html-loader'
